ae6d9eaa2c5cd68e0edf796c869d9d34750c8b0d
roct <694526771@qq.com>
2021-08-28 21:12:05 +0800

    集成tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './style/reset.scss'
+import './style/tailwind.css'
 import router from '@/router/index'
 createApp(App).use(router).mount('#app')
--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <n-button>login</n-button>
+    <div class="login-content-wrapper flex flex-column">
+      <div class="login-content-title text-5xl">欢迎使用</div>
+      <div class="login-content-title">WonderWrap</div>
+      <div class="login-content-desc">中国短视频工业化先驱，打造智能、便捷、高效的工作流</div>
+      <n-button>login</n-button>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
@@ -10,6 +15,6 @@
  * author: roct
  * date: 2:17 下午 2021/8/28
  */
-import {NButton} from "naive-ui";
+import { NButton } from 'naive-ui'
 </script>
 <style scoped lang="scss"></style>
